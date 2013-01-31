50cfe554ff1ac2d09641fb95b48fa6ff7c868525
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-01-30 21:34:17 -0800

    Moving options and platform specific stuff

--- background.js
+++ background.js
@@ -1,7 +1,6 @@
 define(["freedom!bundlemanager",
         "freedom!constants",
-				"freedom!platform/chrome",
-				"freedom!hub"], function(bundleManager, constants, chrome, hub) {
+				"freedom!hub"], function(bundleManager, constants, hub) {
 	"use strict";
 	
 	var storage = hub.req(constants.API._getProvider, constants.CORE.Sender, {
@@ -23,7 +22,7 @@ define(["freedom!bundlemanager",
 		for (var key in values) {
 			if (key == constants.CORE.Bundles) {
 				bundleManager.update(values[key]);
-			}			
+			}
 		}
 	}));
 });
