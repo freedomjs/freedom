deb0c4d3e8234ba13043d64c56117fee522b2a20
Will Scott <willscott@gmail.com>
2013-02-28 09:49:18 -0800

    update all interfaces to not munge

--- interface/core.js
+++ interface/core.js
@@ -1,14 +1,14 @@
 fdom.apis.set("core.view", {
-  show: {type: "method", value: ["object"]},
-  close: {type: "method", value: []},
-  postMessage: {type: "method", value: ["object"]},
+  'show': {type: "method", value: ["object"]},
+  'close': {type: "method", value: []},
+  'postMessage': {type: "method", value: ["object"]},
 
-  message: {type: "event", value: ["object"]},
-  onClose: {type: "event", value: []}
+  'message': {type: "event", value: ["object"]},
+  'onClose': {type: "event", value: []}
 });
 
 fdom.apis.set("core.storage", {
-  set: {type: "method", value: ["string", "string"]},
-  get: {type: "method", value: ["string"]},
-  change: {type: "event", value: ["string"]}
+  'set': {type: "method", value: ["string", "string"]},
+  'get': {type: "method", value: ["string"]},
+  'change': {type: "event", value: ["string"]}
 });
