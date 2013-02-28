baa8a92528eec3724383c28e7bc067cb630b2719
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-02-28 11:56:38 -0800

    interface bug

--- interface/identity.js
+++ interface/identity.js
@@ -1,7 +1,7 @@
 fdom.apis.set("identity", {
   'name': {type: "property", value: "string"},
   'get': {type: "method", value: []},
-  'send': {type: "method", value: ["string", "string"]}
-  'buddylist': {type: "event", value: ["object"]}
-  'message': {type: "event", value["string"]}
+  'send': {type: "method", value: ["string", "string"]},
+  'buddylist': {type: "event", value: ["object"]},
+  'message': {type: "event", value: ["string"]}
 });
