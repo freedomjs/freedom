eadef6a7367cf1e3cb97fbc62fbcdcb591dafee2
Will Scott <willscott@gmail.com>
2013-03-07 10:45:32 -0800

    changing loby to use remoteStorage as an identity provider.

--- demo/lobby/lobby.js
+++ demo/lobby/lobby.js
@@ -1,36 +1,4 @@
 var identity = freedom.identity();
-var transport = freedom['core.transport']();
-var sockId = -1;
-
-freedom.on('send-message', function(val) {
-  identity.send(val.to, val.msg);
-  /**
-  if (sockId == -1) {
-    var promise = transport.create();
-    promise.done(function (data) {
-      sockId = data.id;
-      console.log('Created PeerConnection: '+sockId);
-      //identity.send(val.to, data.offer);
-    });
-  }
-  **/
-});
-
-identity.on('buddylist', function(list) {
-  freedom.emit('recv-buddylist', list);
-});
-
-identity.on('message', function(data) {
-  freedom.emit('recv-message', data.message);
-});
-
-transport.on('onStateChange', function(data) {
-  console.log('State change for id='+data.id+' to '+data.state);
-});
-
-transport.on('onMessage', function(data) {
-  freedom.emit('recv-message', data.id+': '+data.message);
-});
 
 setTimeout(function() {
   //Fetch UID
