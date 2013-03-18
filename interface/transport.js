7d4e0d68148285a5eefd74073a1cebf4af153a47
Will Scott <willscott@gmail.com>
2013-03-17 23:00:31 -0700

    make interface support data

--- interface/transport.js
+++ interface/transport.js
@@ -1,10 +1,16 @@
 fdom.apis.set("core.transport", {
   'create': {type: "method", value: []},
   'accept': {type: "method", value: ["number", "object"]},
-  'send': {type: "method", value: ["number", "blob"]},
+  'send': {type: "method", value: {
+    "header": "object",
+    "data": "blob"
+  }},
   'close': {type: "method", value: ["number"]},
   
   'onStateChange': {type: "event", value: "object"},
-  'onMessage': {type: "event", value: "blob"},
+  'onMessage': {type: "event", value: {
+    "header": "object",
+    "data": "blob"
+  }},
   'onSignal': {type: "event", value: "object"}
 });
