29ede3930f84bc9c9eb721f3d9579747fa2358d7
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-07-31 14:18:52 -0700

    Updates to identity API

--- interface/identity.js
+++ interface/identity.js
@@ -2,24 +2,37 @@ fdom.apis.set("identity", {
   //Stores the 'ID' for logged in user (alice@gmail.com)
   //e.g. var id = identity.id
   'id': {type: "property", value: "string"},
+  //Log into the network
+  //e.g. login(String agent)
+  //Returns {
+  //  'success': 'boolean',
+  //  'message': 'string'
+  //}
+  'login': {type: "method", value: "string"},
   //Gets the profile of a user
   //If id is null, return self
   //e.g. identity.getProfile(String id);
   //Returns {
   //  'card': {
-  //    'id': 'string',       //ID (e.g. alice@gmail.com) username
-  //    'name': 'string',     //Name (e.g. Alice Underpants)
-  //    'imageUrl': 'string', //URL to profile pic
-  //    'status': 'string'    //Status (['messageable', 'online', 'offline'])
-  //  }
-  //  'roster': {             //List of friends
-  //    'id1': {              //NOTE: Key must match 'id' in card
-  //      'id1': 'string',
+  //    'userId': 'string',       //ID (e.g. alice@gmail.com) username
+  //    'name': 'string',         //Name (e.g. Alice Underpants)
+  //    'imageUrl': 'string',     //URL to profile pic
+  //    'devices': [{              //Array of devices
+  //      'deviceId': 'string',   //ID of device (e.g. alice@gmail.com/Android-23nadsv32f)
+  //      'status': 'string'      //Status (['messageable', 'online', 'offline'])
+  //    }, ...]
+  //  },
+  //  'roster': {                 //List of friends
+  //    'user1': {                //NOTE: Key must match 'userId' in card
+  //      'userId': 'string',
   //      'name': 'string',
   //      'imageUrl': 'string',
-  //      'status': 'string'
+  //      'devices': [
+  //        'deviceId': 'string',
+  //        'status': 'string'
+  //      ]
   //    },
-  //    'id2': ...
+  //    'user2': ...
   //  }
   //}
   'getProfile': {type: "method", value: ["string"]},
@@ -30,10 +43,10 @@ fdom.apis.set("identity", {
   //Event on change in profile
   //(includes changes to roster)
   'onChange': {type: "event", value: {
-    'id': 'string',
+    'userId': 'string',
     'name': 'string',
     'imageUrl': 'string',
-    'status': 'string'
+    'devices': ['object']
   }},
   //Event on incoming message
   'onMessage': {type: "event", value: {
