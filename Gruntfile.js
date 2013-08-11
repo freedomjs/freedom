1799b64e1886daaac741ef9c66db6895ca59dddb
Will Scott <willscott@gmail.com>
2013-08-11 15:34:42 -0700

    include jshint in grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,24 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jasmine: {
       freedom: {
-        src: ['src/*.js','src/proxy/*js'],
+        src: ['src/*.js', 'src/proxy/*.js'],
         options: {
           specs: 'spec/*Spec.js'
         }
+      }
+    },
+    jshint: {
+      beforeconcat: ['src/*.js', 'src/proxy/*.js'],
+      options: {
+        '-W069': true
       }
     }
   });
 
   // Load tasks.
   grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default tasks.
-  grunt.registerTask('default', ['jasmine']);
+  grunt.registerTask('default', ['jasmine', 'jshint:beforeconcat']);
 };
