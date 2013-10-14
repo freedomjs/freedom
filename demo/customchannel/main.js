95812333f9d2c9512c1070bac4494dc326c73b3b
Will Scott <willscott@gmail.com>
2013-10-14 19:56:41 +0900

    add demo of sctp-peerconnection

--- demo/customchannel/main.js
+++ demo/customchannel/main.js
@@ -32,10 +32,53 @@ freedom.on('destroy', function(id) {
 });
 
 freedom.on('message', function(id) {
-  freedom.emit('message', 'sending message to ' + id);
-  channels[id].emit('message', 'Message to chan ' + id);
+  if(channels[id].peer) {
+   freedom.emit('message', 'sending message to peer ' + id);
+   channels[id].send({'channelLabel':'test', 'text':'message to peer ' + id});
+ } else {
+    freedom.emit('message', 'sending message to ' + id);
+    channels[id].emit('message', 'Message to chan ' + id);
+  }
+});
+
+freedom.on('peer', function() {
+  var thisid = id;
+  id++;
+  core.createChannel().done(function(cinfo) {
+    core.createChannel().done(function(cinfo, c2info) {
+      var peerEnd1 = freedom['core.sctp-peerconnection']();
+      var peerEnd2 = freedom['core.sctp-peerconnection']();
+
+      peerEnd1.peer = true;
+      channels[thisid] = peerEnd1;
+
+      peerEnd2.on('onReceive', function(str) {
+        freedom.emit('message', "from Peer Connection Round Trip: " + JSON.stringify(str));
+      });
+
+ 
+      c2info.channel.done(function(chan) {
+        channels[thisid].c2 = chan;
+        chan.on('message', function(m) {
+          channels[thisid].c.emit('message', m);
+        });
+      });
+
+      cinfo.channel.done(function(chan) {
+        channels[thisid].c = chan;
+        chan.on('message', function(m) {
+          channels[thisid].c2.emit('message', m);
+        });
+      });
+
+      // Set them up.
+      peerEnd1.setup(cinfo.identifier);
+      peerEnd2.setup(c2info.identifier);
+    }.bind(this, cinfo));
+  });
 });
 
 friend.on('message', function(str) {
   freedom.emit('message', str);
 });
+
