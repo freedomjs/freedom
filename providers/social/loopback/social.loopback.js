2058ca2a8cfcf55a39daa657abf6cedca0552750
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-11-02 21:05:06 -0700

    Updated loopback provider for social API

--- providers/social/loopback/social.loopback.js
+++ providers/social/loopback/social.loopback.js
@@ -11,61 +11,56 @@
  * - 'Other User' is a special buddy that echos what you say back to you
  **/
 
-var window = {};
 var social = freedom.social();
 var STATUS_NETWORK = social.STATUS_NETWORK;
 var STATUS_CLIENT = social.STATUS_CLIENT;
 var NETWORK_ID = 'loopback';
+var USER_ID = 'Test User';
+var CLIENT_ID = 'Test User.0';
 
 function SocialProvider() {
   console.log("Loopback Social provider");
-  this.status = 'offline';
-  this.userId = 'Test User';
-  this.profile = {
-    me: {
-      "Test User": {
-        userId: "Test User",
-        name: "Test User",
-        clients: {'Test User.0': {
-          'clientId':"Test User.0",
-          'network': 'loopback',
-          'status': "messageable"
-        }}
-      }
+  this.roster = {
+    USER_ID: {
+      userId: USER_ID,
+      name: USER_ID,
+      clients: {'Test User.0': {
+        'clientId': CLIENT_ID,
+        'network': NETWORK_ID,
+        'status': STATUS_CLIENT["MESSAGEABLE"]
+      }}
     },
-    'roster': {
-      "Other User": {
-        userId: "Other User",
-        name: "Other User",
-        clients: {'Other User.0':{
-          'clientId': "Other User.0", 
-          'network': "loopback",
-          'status': 'messageable'
-        }}
-      },
-      'Johnny Appleseed': makeRosterEntry('Johnny Appleseed'),
-      'Betty Boop': makeRosterEntry('Betty Boop'),
-      'Big Bird': makeRosterEntry('Big Bird'),
-      'Bugs Bunny': makeRosterEntry('Bugs Bunny'),
-      'Daffy Duck': makeRosterEntry('Daffy Duck'),
-      'Kermit the Frog': makeRosterEntry('Kermit the Frog'),
-      'Minnie Mouse': makeRosterEntry('Minnie Mouse'),
-      'Porky Pig': makeRosterEntry('Porky Pig'),
-      'Swedish Chef': makeRosterEntry('Swedish Chef'),
-      'Yosemite Sam': makeRosterEntry('Yosemite Sam')
-    }
+    "Other User": {
+      userId: "Other User",
+      name: "Other User",
+      clients: {'Other User.0':{
+        'clientId': "Other User.0", 
+        'network': NETWORK_ID,
+        'status': STATUS_CLIENT["MESSAGEABLE"]
+      }}
+    },
+    'Johnny Appleseed': makeRosterEntry('Johnny Appleseed'),
+    'Betty Boop': makeRosterEntry('Betty Boop'),
+    'Big Bird': makeRosterEntry('Big Bird'),
+    'Bugs Bunny': makeRosterEntry('Bugs Bunny'),
+    'Daffy Duck': makeRosterEntry('Daffy Duck'),
+    'Kermit the Frog': makeRosterEntry('Kermit the Frog'),
+    'Minnie Mouse': makeRosterEntry('Minnie Mouse'),
+    'Porky Pig': makeRosterEntry('Porky Pig'),
+    'Swedish Chef': makeRosterEntry('Swedish Chef'),
+    'Yosemite Sam': makeRosterEntry('Yosemite Sam')
   };
   setTimeout((function() {
     this.dispatchEvent('onStatus', {
-      userId: this.userId,
-      network: 'loopback',
-      status: this.status,
+      network: NETWORK_ID,
+      userId: USER_ID,
+      status: STATUS_NETWORK['OFFLINE'],
       message: "Woo!"
     });
   }).bind(this), 0);
 }
 
-var STATUSES = ['messageable', 'online', 'offline'];
+var STATUSES = ['MESSAGEABLE', 'ONLINE', 'OFFLINE'];
 
 function makeRosterEntry(userId, opts) {
   opts = opts || {};
@@ -82,8 +77,8 @@ function makeRosterEntry(userId, opts) {
       var clientId = userId+'/-client'+i;
       clients[clientId] = {
         clientId: clientId,
-        network: "loopback",
-        status: STATUSES[i]
+        network: NETWORK_ID,
+        status: STATUS_CLIENT[STATUSES[i]]
       };
     }
     entry.clients = clients;
@@ -92,69 +87,55 @@ function makeRosterEntry(userId, opts) {
 }
 
 SocialProvider.prototype.login = function(opts, continuation) {
-  this.status = 'online';
-  this.dispatchEvent('onStatus', {
-    userId: this.userId,
-    network: 'loopback',
-    status: this.status,
+  var ret = {
+    network: NETWORK_ID,
+    userId: USER_ID,
+    status: STATUS_NETWORK["ONLINE"],
     message: "Woo!"
-  });
-  this.dispatchEvent('onChange', this.profile.me[this.userId]);
-  for (var id in this.profile.roster) {
-    if (this.profile.roster.hasOwnProperty(id)) {
-      this.dispatchEvent('onChange', this.profile.roster[id]);
+  };
+  for (var id in this.roster) {
+    if (this.roster.hasOwnProperty(id)) {
+      this.dispatchEvent('onChange', this.roster[id]);
     }
   }
-  continuation();
+  this.dispatchEvent('onStatus', ret);
+  continuation(ret);
 };
 
-SocialProvider.prototype.getProfile = function(id, continuation) {
-  //TODO get profiles for other users
-  if (id == undefined) {
-    continuation(this.profile);
-  } else if (this.profile.me[id]) {
-    continuation(this.profile);
-  } else if (this.profile.roster[id]) {
-    continuation({me: this.profile.roster[id], roster: {}});
-  }
+SocialProvider.prototype.getRoster = function(continuation) {
+  continuation(this.roster);
 };
 
 // Send a message to someone.
 SocialProvider.prototype.sendMessage = function(to, msg, continuation) {
   this.dispatchEvent('onMessage', {
     fromUserId: "Other User",
-    fromClientId: "Other User.0", 
-    toUserId: "Test User",
-    toClientId: "Test User.0",
-    network: "loopback",
+    fromClientId: "Other User.0",
+    toUserId: USER_ID,
+    toClientId: CLIENT_ID,
+    network: NETWORK_ID,
     message: msg
   });
   continuation();
 };
 
-SocialProvider.prototype.logout = function(userId, networkName, continuation) {
-  this.status = 'offline'; 
-  this.dispatchEvent('onStatus', {
-    userId: this.userId,
-    network: 'loopback',
-    status: this.status,
+SocialProvider.prototype.logout = function(opts, continuation) {
+  var ret = {
+    network: NETWORK_ID,
+    userId: USER_ID,
+    status: STATUS_NETWORK['OFFLINE'],
     message: 'Woo!'
-  });
-  var card = JSON.parse(JSON.stringify(this.profile.me[this.userId]));
-  card.clients = {};
-  this.dispatchEvent('onChange', card);
-  for (var id in this.profile.roster) {
-    if (this.profile.roster.hasOwnProperty(id)) {
-      var card = JSON.parse(JSON.stringify(this.profile.roster[id]));
+  };
+  // Remove all clients in the roster and emit these changes
+  for (var id in this.roster) {
+    if (this.roster.hasOwnProperty(id)) {
+      var card = JSON.parse(JSON.stringify(this.roster[id]));
       card.clients = {};
       this.dispatchEvent('onChange', card);
     }
   }
-  continuation({
-    userId: this.userId,
-    success: true,
-    message: 'Null logout process'
-  });
+  this.dispatchEvent('onStatus', ret);
+  continuation(ret);
 };
 
 social.provideAsynchronous(SocialProvider);
