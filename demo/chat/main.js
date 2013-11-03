7e62e8e17fa2513c344b5c40c98d018417f86766
Raymond Cheng <RaymondYSCheng@gmail.com>
2013-11-03 13:11:12 -0800

    Chat demo properly updates when user goes offline

--- demo/chat/main.js
+++ demo/chat/main.js
@@ -1,15 +1,28 @@
 /**
- * 
+ * Chat demo backend.
+ * Because the Social API provides message passing primitives,
+ * this backend simply forwards messages between the front-end and our Social provider
+ * Note that you should be able to plug-and-play a variety of social providers
+ * and still have a working demo
+ *
  **/
 
 var social = freedom.socialprovider();
-var activeJid;
-var roster = {};
+var roster = {};    //Keep track of the roster
 
+/** 
+ * on a 'send-message' event from the parent (the outer page)
+ * Just forward it to the Social provider
+ **/
 freedom.on('send-message', function(val) {
   social.sendMessage(val.to, val.message);
 });
 
+/**
+ * on a 'onStatus' from the Social provider
+ * Just forward it to the outer page
+ * If we see our userId, send that as a separate event
+ **/
 social.on('onStatus', function(msg) {
   freedom.emit('onStatus', msg);
   if (msg.userId) {
@@ -17,23 +30,51 @@ social.on('onStatus', function(msg) {
   }
 });
 
+/**
+ * on an 'onMessage' event from the Social provider
+ * Just forward it to the outer page
+ */
 social.on('onMessage', function(data) {
   freedom.emit('recv-message', data.message);
 });
 
+/**
+ * On roster changes, let's keep track of them
+ **/
 social.on('onChange', function(data) {
-  console.log("!!!!" + JSON.stringify(data));
   roster[data.userId] = data;
+  // Iterate over our roster and just send over userId's where there is at least 1 client online
   var buddylist = [];
   for (var k in roster) {
-    if (roster.hasOwnProperty(k)) {
+    if (roster.hasOwnProperty(k) && hasOnlineClient(roster[k])) {
       buddylist.push(k);
     }
   }
   freedom.emit('recv-buddylist', buddylist);
 });
 
+/**
+ * Iterate over a <user card> (see interface/social.js for schema)
+ * and check for a client that's online
+ * 
+ * @method hasOnlineClient
+ * @param {Object} data - <user card>
+ * @return {Boolean} - true if there's at least 1 online client 
+ **/
+function hasOnlineClient(data) {
+  if (data.clients) {
+    for (var k in data.clients) {
+      if (data.clients.hasOwnProperty(k) && 
+          data.clients[k].status &&
+          data.clients[k].status !== social.STATUS_CLIENT['OFFLINE']) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
+// Try to log in once we start
 var onload = function() {
   //Fetch UID
   social.login({
