99f78f972d03a03652652866e45dbacb61692b40
Nick Martindell <nickmart@uw.edu>
2013-11-19 15:10:44 -0800

    Tests display in a full tab rather than window

--- tools/ChromeTestRunner/background.js
+++ tools/ChromeTestRunner/background.js
@@ -1,8 +1,3 @@
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('window.html', {
-    'bounds': {
-      'width': 700,
-      'height': 500
-    }
-  });
+  window.open('window.html');
 });
