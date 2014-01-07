c6160bfd71fd325a7816a53917789e218d51041a
Raymond Cheng <RaymondYSCheng@gmail.com>
2014-01-06 18:57:14 -0800

    Started new transport API

--- interface/transport.js
+++ interface/transport.js
@@ -1,15 +1,29 @@
+/**
+ * TRANSPORT API
+ *
+ * API for peer-to-peer communication
+ * Useful for sending large binary data between instances
+ **/
 fdom.apis.set("transport", {
-  // open is called to bind a freedom channel identifier (called a proxy) to act
-  // as a signalling channel to setup a data connection. If the provided boolean
-  // is true, then the open call will initiate the setup of the connection when
-  // the transport setup is asynchronous.
-  'open': {type: "method", value: ["proxy", "bool"]},
+  /**
+  * open is called to bind a freedom channel identifier (called a proxy) to act
+  * as a signalling channel to setup a data connection. If the provided boolean
+  * is true, then the open call will initiate the setup of the connection when
+  *
+  * @method setup
+  * @param {proxy} channel - signalling channel
+  * @param {bool} init - true if initiates connection
+  * @return nothing
+  **/ 
+  'setup': {type: "method", value: ["proxy", "bool"]},
 
   // Send data down the channel with name specified by the string.
-  'send': {type: "method", value: ["string", "data"]},
+  'send': {type: "method", value: ["string", "buffer"]},
 
   // TODO: refactor, naming this onMessage.
-  'message': {type: "event", value: "data"},
+  'onData': {type: "event", value: "buffer"},
+
   'close': {type: "method", value: []},
+
   'onClose': {type: "event", value: []}
 });
