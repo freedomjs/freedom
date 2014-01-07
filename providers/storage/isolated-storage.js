8f2b8177916d74b0921b52431c3c549714299335
Raymond Cheng <RaymondYSCheng@gmail.com>
2014-01-06 16:32:20 -0800

    Isolated storage provider

--- providers/storage/isolated-storage.js
+++ providers/storage/isolated-storage.js
@@ -5,20 +5,34 @@
  *    e.g. Both modules A and B use this storage provider.
  *    They cannot access each other's keys
  **/
+window = {};
 
 function StorageProvider() {
+  console.log("Isolated Storage Provider, running in worker " + self.location.href);
+  window.current = this;
   this.core = freedom['core']();
   this.store = freedom['core.storage']();
-  // This magic needs to be replaced with something deterministic that
-  // uniquely identifies its place in the dependency tree
-  this.magic = self.location.href;
-  console.log("Isolated Storage Provider, running in worker " + self.location.href);
+  this.magic = "";
+  this.queue = [];
+
+  this.core.getId().done((function (val) {
+    for (var i =0; i < val.length; i++) {
+      this.magic += val[i] + ";";
+    }
+    this.flushQueue();
+  }).bind(this));
 }
 
 StorageProvider.prototype.keys = function(continuation) {
+  if (this.magic === "") {
+    this.pushQueue("keys", null, null, continuation);
+    return;
+  }
+
   var promise = this.store.keys();
   promise.done(function(val) {
     var result = [];
+    //Check that our magic has been initialized
     //Only return keys in my partition
     for (var i = 0; i < val.length; i++) {
       if (this.isMyKey(val[i])) {
@@ -30,16 +44,31 @@ StorageProvider.prototype.keys = function(continuation) {
 };
 
 StorageProvider.prototype.get = function(key, continuation) {
+  if (this.magic === "") {
+    this.pushQueue("get", key, null, continuation);
+    return;
+  } 
+  
   var promise = this.store.get(this.toStoredKey(key));
   promise.done(continuation);
 };
 
 StorageProvider.prototype.set = function(key, value, continuation) {
+  if (this.magic === "") {
+    this.pushQueue("set", key, value, continuation);
+    return;
+  }
+
   var promise = this.store.set(this.toStoredKey(key), value);
   promise.done(continuation);
 };
 
 StorageProvider.prototype.remove = function(key, continuation) {
+  if (this.magic === "") {
+    this.pushQueue("remove", key, null, continuation);
+    return;
+  }
+  
   var promise = this.store.remove(this.toStoredKey(key));
   promise.done(continuation);
 };
@@ -58,6 +87,36 @@ StorageProvider.prototype.clear = function(continuation) {
 };
 
 /** INTERNAL METHODS **/
+//Insert call into queue
+StorageProvider.prototype.pushQueue = function(method, key, value, continuation) {
+  this.queue.push({
+    cmd: method,
+    key: key,
+    value: value,
+    cont: continuation
+  });
+};
+
+//Flush commands in queue
+StorageProvider.prototype.flushQueue = function() {
+  for (var i = 0; i < this.queue.length; i++) {
+    var elt = this.queue[i];
+    if (elt.cmd == "keys") {
+      this.keys(elt.cont);
+    } else if (elt.cmd == "get") {
+      this.get(elt.key, elt.cont);
+    } else if (elt.cmd == "set") {
+      this.set(elt.key, elt.value, elt.cont);
+    } else if (elt.cmd == "remove") {
+      this.remove(elt.key, elt.cont);
+    } else {
+      console.error("Isolated Storage: unrecognized command " + JSON.stringify(elt));
+    }
+  }
+
+  this.queue = [];
+};
+
 // From caller's key => stored key
 // e.g. 'keyA' => 'partition1+keyA'
 StorageProvider.prototype.toStoredKey = function(key) {
