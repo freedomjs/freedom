b10a4658df7b02128d6696b665597123630c72a1
Nick Martindell <nickmart@uw.edu>
2014-01-08 18:41:25 -0800

    Added support to Chrome Test Runner for new spec file naming convention. (*Spec.js --> *.spec.js)

--- tools/chromeTestRunner.sh
+++ tools/chromeTestRunner.sh
@@ -39,7 +39,13 @@ done
 # Assemble all the spec files
 SPEC_SCRIPTS=""
 pushd "$FREEDOM_ROOT_DIR" > /dev/null
-for FILE in `find . -iname "*spec.js"`
+# Files in /spec/ ending with Spec.js (old format)
+for FILE in `find spec -name "*Spec.js"`
+do
+	SPEC_SCRIPTS="$SPEC_SCRIPTS<script type=\"text/javascript\" src=\"$FILE\"></script>"
+done
+# Files anywhere ending with .spec.js (new format)
+for FILE in `find . -name "*.spec.js"`
 do
 	SPEC_SCRIPTS="$SPEC_SCRIPTS<script type=\"text/javascript\" src=\"$FILE\"></script>"
 done
