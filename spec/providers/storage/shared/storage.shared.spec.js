9f23634223e0c54f85c489c92e709d80eb25e7c4
Raymond Cheng <RaymondYSCheng@gmail.com>
2014-01-15 16:51:23 -0800

    updated storage specs and fixed some isolated storage bugs

--- spec/providers/storage/shared/storage.shared.spec.js
+++ spec/providers/storage/shared/storage.shared.spec.js
@@ -1,12 +1,14 @@
-describe("storage.shared.json", function() {
+describe("storage.shared/isolated.json", function() {
   var proxy;
+  var TIMEOUT = 1000;
   beforeEach(function() {
-    proxy = createProxyFor("providers/storage/shared/storage.shared.json", "storage");
+    //proxy = createProxyFor("providers/storage/shared/storage.shared.json", "storage");
+    proxy = createProxyFor("providers/storage/isolated/storage.isolated.json", "storage");
   });
 
-  it("Returns Keys", function() {
-    var val = undefined;
+  it("sets and gets keys", function() {
     var p = proxy();
+    var val = undefined;
 
     runs(function() {
       p.set('key', 'myvalue').done(function() {
@@ -17,7 +19,7 @@ describe("storage.shared.json", function() {
     });
     waitsFor("Key to be round-tripped", function() {
       return val != undefined;
-    }, 1000);
+    }, TIMEOUT);
     runs(function() {
       expect(val).toEqual('myvalue');
       p.clear().done(function() {
@@ -26,178 +28,94 @@ describe("storage.shared.json", function() {
     });
     waitsFor('Cleanup to finish', function() {
       return val == undefined;
-    }, 1000);
+    }, TIMEOUT);
   });
-});
 
-describe("/providers/storage/shared/storage.shared.json", function() {
-  var ASYNC_TIMEOUT = 4000;
-  var freedom_src;
+  it("lists keys that have been set", function() {
+    var p = proxy();
+    var keys = undefined;
 
-  var freedom, dir;
-  beforeEach(function() {
-    freedom_src = getFreedomSource();
-    var global = {console: {log: function() {}}};
-    setupResolvers();
-    var path = window.location.href,
-        dir_idx = path.lastIndexOf('/');
-    dir = path.substr(0, dir_idx) + '/';
+    runs(function() {
+      p.set('k1', 'v1').done(function() {
+        p.set('k2', 'v2').done(function() {
+          p.keys().done(function(ret) {
+            keys = ret;
+          });
+        });
+      });
+    });
+    waitsFor("keys to be set", function() {
+      return keys != undefined;
+    }, TIMEOUT);
+    runs(function() {
+      expect(keys.length).toEqual(2);
+      expect(keys).toContain('k1');
+      expect(keys).toContain('k2');
+      p.clear().done(function() {
+        keys = undefined;
+      });
+    });
+    waitsFor("cleanup keys", function() {
+      return keys == undefined;
+    }, TIMEOUT);
+  });
 
-    freedom = setup(global, undefined, {
-      manifest: "relative://spec/helper/providers.json",
-      portType: 'Frame',
-      inject: dir + "node_modules/es5-shim/es5-shim.js",
-      src: freedom_src
+  it("clears the store", function() {
+    var p = proxy();
+    var val = undefined;
+
+    runs(function() {
+      p.set('key', 'value').done(function() {
+        p.clear().done(function() {
+          p.keys().done(function(ret) {
+            val = ret;
+          });
+        });
+      });
+    });
+    waitsFor("keys to be set", function() {
+      return val != undefined;
+    }, TIMEOUT);
+    runs(function() {
+      expect(val.length).toEqual(0);
     });
   });
+
+  it("shares data between different instances", function() {
+    var p1 = proxy();
+    var p2 = proxy();
+    var isKeySet = false;
+    var val = undefined;
+
+    runs(function() {
+      p1.set("key", "value").done(function() {
+        isKeySet = true;
+      });
+    });
+    waitsFor("keys should be set", function() {
+      return isKeySet == true;
+    }, TIMEOUT);
+    runs(function() {
+      p2.get("key").done(function(ret) {
+        val = ret;
+      });
+    });
+    waitsFor("keys retrieved", function() {
+      return val != undefined;
+    });
+    runs(function() {
+      expect(val).toEqual("value");
+      p1.clear().done(function () {
+        p2.clear().done(function () {
+          val = undefined;
+        });
+      });
+    });
+    waitsFor("cleanup keys", function() {
+      return val == undefined;
+    }, TIMEOUT);
+
   
-  afterEach(function() {
-    var frames = document.getElementsByTagName('iframe');
-    for (var i = 0; i < frames.length; i++) {
-      frames[i].parentNode.removeChild(frames[i]);
-    }
-  });
-
-  it("lets keys be set", function() {
-    var cb = jasmine.createSpy('cb');
-    var called = false;
-    runs(function() {
-      freedom.on('return', cb);
-      freedom.on('return', function() {
-        called = true;
-      });
-      freedom.emit('call', JSON.stringify({
-        "method": "set",
-        "args": ["testKey", "testVal"]
-      }));
-    });
-
-    waitsFor(function() {
-      return called;
-    }, "RPC 'set' to storage.json module should return", ASYNC_TIMEOUT);
-
-    runs(function() {
-      expect(cb).toHaveBeenCalledWith(undefined);
-    });
-  });
-
-  it("lets keys be retrieved", function() {
-    var cb = jasmine.createSpy('cb');
-    var called = false;
-    runs(function() {
-      freedom.on('return', cb);
-      freedom.on('return', function() {
-        called = true;
-      });
-      freedom.emit('call', JSON.stringify({
-        "method": "set",
-        "args": ["testKey", "testVal"]
-      }));
-    });
-
-    waitsFor(function() {
-      return called;
-    }, "RPC 'set' to storage.json module should return", ASYNC_TIMEOUT);
-
-    runs(function() {
-      expect(cb).toHaveBeenCalledWith(JSON.undefined);
-    });
-
-    runs(function() {
-      called = false;
-      freedom.emit('call', JSON.stringify({
-        "method": "get",
-        "args": ["testKey"]
-      }));
-    });
-
-    waitsFor(function() {
-      return called;
-    }, "RPC 'get' to storage.json module should return", ASYNC_TIMEOUT);
-
-    runs(function() {
-      expect(cb).toHaveBeenCalledWith(JSON.stringify('testVal'));
-    });
-  });
-
-  it("lets keys be listed", function() {
-    var cb = jasmine.createSpy('cb');
-    var called = false;
-    runs(function() {
-      freedom.on('return', cb);
-      freedom.on('return', function() {
-        called = true;
-      });
-      freedom.emit('call', JSON.stringify({
-        "method": "set",
-        "args": ["testKey", "testVal"]
-      }));
-    });
-
-    waitsFor(function() {
-      return called;
-    }, "RPC 'set' to storage.json module should return", ASYNC_TIMEOUT);
-
-    runs(function() {
-      expect(cb).toHaveBeenCalledWith(JSON.undefined);
-    });
-
-    runs(function() {
-      called = false;
-      freedom.emit('call', JSON.stringify({
-        "method": "keys",
-        "args": []
-      }));
-    });
-
-    waitsFor(function() {
-      return called;
-    }, "RPC 'keys' to storage.json module should return", ASYNC_TIMEOUT);
-
-    runs(function() {
-      expect(cb).toHaveBeenCalledWith(JSON.stringify(['testKey']));
-    });
-  });
-
-  it("clears the storage space", function(){
-    var cb = jasmine.createSpy('cb');
-    var called = false;
-
-    // Clear the storage space
-    runs(function() {
-      freedom.on('return', cb);
-      freedom.on('return', function() {
-        called = true;
-      });
-      freedom.emit('call', JSON.stringify({
-        "method": "clear",
-        "args": []
-      }));
-    });
-
-    waitsFor(function() {
-      return called;
-    }, "RPC 'clear' to storage.json module should return", ASYNC_TIMEOUT);
-
-    runs(function() {
-      expect(cb).toHaveBeenCalledWith(JSON.undefined);
-    });
-
-    // Check it is cleared
-    runs(function() {
-      called = false;
-      freedom.emit('call', JSON.stringify({
-        "method": "keys",
-        "args": []
-      }));
-    });
-
-    waitsFor(function() {
-      return called;
-    }, "RPC 'keys' to storage.json module should return", ASYNC_TIMEOUT);
-
-    runs(function() {
-      expect(cb).toHaveBeenCalledWith(JSON.stringify([]));
-    });
   });
 });
+
