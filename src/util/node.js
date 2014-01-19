e603a2fe76e46847f57be09e7c262208e065edb0
Will Scott <willscott@gmail.com>
2014-01-19 12:58:19 -0800

    bit of progress on node entrypoint.

--- src/util/node.js
+++ src/util/node.js
@@ -1,3 +1,4 @@
+
 /*jslint indent:2,white:true,sloppy:true,node:true */
 /*
  * freedom.js Node runtime
@@ -5,6 +6,21 @@
 
 'use strict';
 
-var fdom = module.exports = {};
+var sources = [
+  '/..',
+  '/../proxy',
+  '/../../interface'
+];
 
-require('../../freedom.js');
+sources.forEach(function(dir) {
+  require('fs').readdirSync(__dirname + dir).forEach(function(file) {
+    if (file.match(/.+\.js/) !== null) {
+      require(__dirname + dir + '/' + file);
+    }
+  });
+});
+
+module.exports.freedom = fdom.setup(global, undefined, {
+  portType: 'nodeVM'
+});
+delete global.fdom;
