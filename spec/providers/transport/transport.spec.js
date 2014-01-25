9ddef2543c6cffa5e1c702ac1527de9e1e4db64f
Raymond Cheng <ryscheng@cs.washington.edu>
2014-01-24 20:14:51 -0800

    Blowing away old comments and old router.py

--- spec/providers/transport/transport.spec.js
+++ spec/providers/transport/transport.spec.js
@@ -89,69 +89,6 @@ var TRANSPORT_SPEC = function(transportId) { return function() {
   });
 
 
-/**
-  it("can setup", function() {
-    var p1 = proxy();
-    var p2 = proxy();
-    var core = undefined; 
-    var chan1 = undefined;
-    var chan2 = undefined;
-    var val = undefined;
-
-    waitsFor("freedom to be set", function() {
-      return typeof freedom !== "undefined";
-    }, TIMEOUT);
-
-    runs(function (){
-      core = freedom.core();
-      core.createChannel().done(function (chan) {
-        console.log("Chan1 done");
-        chan.channel.on('ready', function(){console.log("p1ready");});
-        chan.channel.on('message', function(msg) {
-          console.log("2->1")
-          console.log(msg);
-          chan2.channel.emit("message", msg);
-        });
-        chan1 = chan;
-      });
-      core.createChannel().done(function (chan) {
-        console.log("Chan2 done");
-        chan.channel.on('ready', function(){console.log("p2ready");});
-        chan.channel.on('message', function(msg) {
-          console.log("1->2")
-          console.log(msg);
-          chan1.channel.emit("message", msg);
-        });
-        chan2 = chan;
-      });
-    });
-    waitsFor("create channels", function() {
-      return ((chan1 !== undefined) && (chan2 !== undefined));
-    }, TIMEOUT);
-
-    runs(function() {
-      p1.on('onData', function(msg) {
-        console.warn(msg);
-        val = msg;
-      });
-      p1.setup("p1", chan1.identifier);
-      p2.setup("p2", chan2.identifier);
-      p1.send('tag', 'test');
-    });
-    waitsFor("value to transmit", function() {
-      return val != undefined;
-    }, TIMEOUT);
-    
-    runs(function() {
-      expect(val.tag).toEqual('test');
-    });
-
-
-
-
-  });
-
-**/
 
 }};
 
