7c128b59747a504e7f8a620881f04ef5bf2712b6
Will Scott <willscott@gmail.com>
2014-01-29 17:46:30 -0800

    Finish isolated storage test

--- spec/providers/storage/isolatedstorage.unit.spec.js
+++ spec/providers/storage/isolatedstorage.unit.spec.js
@@ -19,4 +19,32 @@ describe("providers.storage.isolated", function() {
     expect(provider.store.keys).toHaveBeenCalled();
     expect(d).toHaveBeenCalledWith(['Test']);
   });
+
+  it("gets saved items", function() {
+    var d = jasmine.createSpy('get');
+    provider.get('mykey', d);
+    expect(d).toHaveBeenCalledWith('value');
+    expect(provider.store.get).toHaveBeenCalledWith('myId;mykey');
+  });
+
+  it("sets items", function() {
+    var d = jasmine.createSpy('set');
+    provider.set('mykey', 'myval', d);
+    expect(d).toHaveBeenCalled();
+    expect(provider.store.set).toHaveBeenCalledWith('myId;mykey', 'myval');
+  });
+
+  it("Removes items", function() {
+    var d = jasmine.createSpy('remove');
+    provider.remove('mykey', d);
+    expect(d).toHaveBeenCalled();
+    expect(provider.store.remove).toHaveBeenCalledWith('myId;mykey');
+  });
+
+  it("Clears storage", function() {
+    var d = jasmine.createSpy('clear');
+    provider.clear(d);
+    expect(d).toHaveBeenCalled();
+    expect(provider.store.remove).toHaveBeenCalled();
+  });
 });
