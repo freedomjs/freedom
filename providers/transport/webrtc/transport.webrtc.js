f2de60e9793d36563c7099b951d30311f24360c0
Paul Ellenbogen <epaul9@cs.washington.edu>
2014-02-11 13:59:15 -0800

    Add transport unit tests. Fixes #40

--- providers/transport/webrtc/transport.webrtc.js
+++ providers/transport/webrtc/transport.webrtc.js
@@ -12,7 +12,7 @@ var stun_servers = [
  */
 
 function TransportProvider() {
-  console.log("TransportProvider: running in worker " + self.location.href);
+  // console.log("TransportProvider: running in worker " + self.location.href);
   this.name = null;
   this.pc = freedom['core.peerconnection']();
   this.pc.on('onReceived', this.onData.bind(this));
@@ -23,14 +23,14 @@ function TransportProvider() {
 // The argument |channelId| is a freedom communication channel id to use
 // to open a peer connection. 
 TransportProvider.prototype.setup = function(name, channelId, continuation) {
-  console.log("TransportProvider.setup." + name);
+  // console.log("TransportProvider.setup." + name);
   this.name = name;
   var promise = this.pc.setup(channelId, name, stun_servers);
   promise.done(continuation);
 };
 
 TransportProvider.prototype.send = function(tag, data, continuation) {
-  console.log("TransportProvider.send." + this.name);
+  // console.log("TransportProvider.send." + this.name);
   if (this._tags.indexOf(tag) >= 0) {
     var promise = this.pc.send({"channelLabel": tag, "buffer": data});
     promise.done(continuation);
@@ -50,7 +50,7 @@ TransportProvider.prototype.close = function(continuation) {
 
 // Called when the peer-connection receives data, it then passes it here.
 TransportProvider.prototype.onData = function(msg) {
-  console.log("TransportProvider.prototype.message: Got Message:" + JSON.stringify(msg));
+  // console.log("TransportProvider.prototype.message: Got Message:" + JSON.stringify(msg));
   if (msg.buffer) {
     this.dispatchEvent('onData', {
       "tag": msg.channelLabel, 
