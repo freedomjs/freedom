d950173c9d207aa2c4adca61f0cfaf8fc652a8c2
Raymond Cheng <ryscheng@cs.washington.edu>
2014-05-24 20:09:34 -0700

    adding a story reporter

--- karma.conf.js
+++ karma.conf.js
@@ -53,7 +53,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['dots', 'progress', 'coverage', 'saucelabs'],
+    reporters: ['progress', 'coverage', 'saucelabs', 'unicorn', 'story'],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
