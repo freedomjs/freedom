276998adc1a8523a91ef8aaf1ab25bb3be75fd66
Serene H <keroserene+git@gmail.com>
2014-06-23 14:00:10 -0400

    consistency with single quotes

--- interface/core.js
+++ interface/core.js
@@ -1,123 +1,123 @@
 /*globals fdom:true */
 /*jslint indent:2,sloppy:true */
 
-fdom.apis.set("core", {
-  'createChannel': {type: "method", value: [], ret: {
-    channel: "proxy",
-    identifier: "string"
+fdom.apis.set('core', {
+  'createChannel': {type: 'method', value: [], ret: {
+    channel: 'proxy',
+    identifier: 'string'
   }},
-  'bindChannel': {type: "method", value: ["string"], ret: "proxy"},
-  'getId': {type: "method", value: [], ret: ["array", "string"]}
+  'bindChannel': {type: 'method', value: ['string'], ret: 'proxy'},
+  'getId': {type: 'method', value: [], ret: ['array', 'string']}
 });
 
-fdom.apis.set("core.view", {
-  'open': {type: "method", value: ["string", {
-    'file': "string",
-    'code': "string"
+fdom.apis.set('core.view', {
+  'open': {type: 'method', value: ['string', {
+    'file': 'string',
+    'code': 'string'
   }]},
-  'show': {type: "method", value: []},
-  'close': {type: "method", value: []},
-  'postMessage': {type: "method", value: ["object"]},
+  'show': {type: 'method', value: []},
+  'close': {type: 'method', value: []},
+  'postMessage': {type: 'method', value: ['object']},
 
-  'message': {type: "event", value: "object"},
-  'onClose': {type: "event", value: []}
+  'message': {type: 'event', value: 'object'},
+  'onClose': {type: 'event', value: []}
 });
 
-fdom.apis.set("core.storage", {
-  'keys': {type: "method", value: [], ret: ["array", "string"]},
-  'get': {type: "method", value: ["string"], ret: "string"},
-  'set': {type: "method", value: ["string", "string"], ret: "string"},
-  'remove': {type: "method", value: ["string"], ret: "string"},
-  'clear': {type: "method", value: []}
+fdom.apis.set('core.storage', {
+  'keys': {type: 'method', value: [], ret: ['array', 'string']},
+  'get': {type: 'method', value: ['string'], ret: 'string'},
+  'set': {type: 'method', value: ['string', 'string'], ret: 'string'},
+  'remove': {type: 'method', value: ['string'], ret: 'string'},
+  'clear': {type: 'method', value: []}
 });
 
 // A TCP Socket.
-fdom.apis.set("core.tcpsocket", {
+fdom.apis.set('core.tcpsocket', {
   // Sockets may be constructed bound to a pre-existing id, as in the case of
   // interacting with a socket accpeted by a server.  If no Id is specified, a
   // new socket will be created, which can be either connect'ed or listen'ed.
   'constructor': {
-    value: ["number"]
+    value: ['number']
   },
 
   // Get info about a socket.  Tells you whether the socket is active and
   // available host information.
   'getInfo': {
-    type: "method",
+    type: 'method',
     value: [],
     ret: {
-      "connected": "boolean",
-      "localAddress": "string",
-      "localPort": "number",
-      "peerAddress": "string",
-      "peerPort": "number"
+      'connected': 'boolean',
+      'localAddress': 'string',
+      'localPort': 'number',
+      'peerAddress': 'string',
+      'peerPort': 'number'
     }
   },
 
   // Close a socket. Will Fail if the socket is not connected or already
   // closed.
   'close': {
-    type: "method",
+    type: 'method',
     value: [],
     err: {
-      "errcode": "string",
-      "message": "string"
+      'errcode': 'string',
+      'message': 'string'
     }
   },
 
   // Receive notification that the socket has disconnected.
-  'onDisconnect': {type: "event", value: {
-    "errcode": "string",
-    "message": "string"
+  'onDisconnect': {type: 'event', value: {
+    'errcode': 'string',
+    'message': 'string'
   }},
   
   // Connect to a host and port.
   // Fails with an error if connection fails.
   'connect': {
-    type: "method",
-    value: ["string", "number"],
+    type: 'method',
+    value: ['string', 'number'],
     err: {
-      "errcode": "string",
-      "message": "string"
+      'errcode': 'string',
+      'message': 'string'
     }
   },
   
   // Write buffer data to a socket.
   // Fails with an error if write fails.
   'write': {
-    type: "method",
-    value: ["buffer"],
+    type: 'method',
+    value: ['buffer'],
     err: {
-      "errcode": "string",
-      "message": "string"
+      'errcode': 'string',
+      'message': 'string'
     }
   },
 
   // Receive data on a connected socket.
   'onData': {
-    type: "event",
-    value: {"data": "buffer"}
+    type: 'event',
+    value: {'data': 'buffer'}
   },
 
   // Listen as a server at a specified host and port.
   // After calling listen the client should listen for 'onConnection' events.
   // Fails with an error if errors occur while binding or listening.
   'listen': {
-    type: "method",
-    value: ["string", "number"],
+    type: 'method',
+    value: ['string', 'number'],
     err: {
-      "errcode": "string",
-      "message": "string"
+      'errcode': 'string',
+      'message': 'string'
     }
   },
 
   // Receive a connection.
   // The socket parameter may be used to construct a new socket.
   // Host and port information provide information about the remote peer.
-  'onConnection': {type: "event", value: {
-    "socket": "number",
-    "host": "string",
-    "port": "number"
+  'onConnection': {type: 'event', value: {
+    'socket': 'number',
+    'host': 'string',
+    'port': 'number'
   }}
 });
 
@@ -188,127 +188,127 @@ fdom.apis.set('core.udpsocket', {
   }
 });
 
-fdom.apis.set("core.runtime", {
-  'createApp': {type: "method", value: ["string", "proxy"]},
-  'resolve': {type: "method", value: ["string", "string"]},
-  'needFile': {type: 'event', value: ["string", "string"]}
+fdom.apis.set('core.runtime', {
+  'createApp': {type: 'method', value: ['string', 'proxy']},
+  'resolve': {type: 'method', value: ['string', 'string']},
+  'needFile': {type: 'event', value: ['string', 'string']}
 });
 
 fdom.apis.set('core.echo', {
-  'setup': {type: "method", value: ["string"]},
-  'send': {type: "method", value: ["string"]},
-  'message': {type: "event", value: "string"}
+  'setup': {type: 'method', value: ['string']},
+  'send': {type: 'method', value: ['string']},
+  'message': {type: 'event', value: 'string'}
 });
 
 
 fdom.apis.set('core.peerconnection', {
   // Setup the link to the peer and options for this peer connection.
   'setup': {
-    type: "method",
+    type: 'method',
     value: [
       // The freedom.js channel identifier used to setup a signalling chanel.
-      "string",
+      'string',
       // The peerName, used debugging and console messages.
-      "string",
+      'string',
       // The list of STUN servers to use.
       // The format of a single entry is stun:HOST:PORT, where HOST
       // and PORT are a stun server hostname and port, respectively.
-      ["array", "string"],
+      ['array', 'string'],
       // Whether to immediately initiate a connection before fulfilling return
       // promise.
-      "boolean"
+      'boolean'
     ]
   },
 
   // Send a message to the peer.
-  'send': {type: "method", value: [{
+  'send': {type: 'method', value: [{
     // Data channel id. If provided, will be used as the channel label.
     // The behavior is undefined if the channel label doesn't exist.
-    "channelLabel": "string",
+    'channelLabel': 'string',
     // One of the bellow should be defined; this is the data to send.
-    "text": "string",
-    "binary": "blob",
-    "buffer": "buffer"
+    'text': 'string',
+    'binary': 'blob',
+    'buffer': 'buffer'
   }]},
 
   // Called when we get a message from the peer.
-  'onReceived': {type: "event", value: {
+  'onReceived': {type: 'event', value: {
     // The label/id of the data channel.
-    "channelLabel": "string",
+    'channelLabel': 'string',
     // One the below will be specified.
-    "text": "string",
-    "binary": "blob",
-    "buffer": "buffer"
+    'text': 'string',
+    'binary': 'blob',
+    'buffer': 'buffer'
   }},
 
   // Open the data channel with this label.
-  'openDataChannel': {type: "method", value: ["string"]},
+  'openDataChannel': {type: 'method', value: ['string']},
   // Close the data channel with this label.
-  'closeDataChannel': {type: "method", value: ["string"]},
+  'closeDataChannel': {type: 'method', value: ['string']},
 
   // A channel with this id has been opened.
-  'onOpenDataChannel': {type: "event", value: {"channelId": "string"}},
+  'onOpenDataChannel': {type: 'event', value: {'channelId': 'string'}},
   // The channale with this id has been closed.
-  'onCloseDataChannel': {type: "event", value: {"channelId": "string"}},
+  'onCloseDataChannel': {type: 'event', value: {'channelId': 'string'}},
 
-  // Returns the number of bytes that have queued using "send", but not
+  // Returns the number of bytes that have queued using 'send', but not
   // yet sent out. Currently just exposes:
   // http://www.w3.org/TR/webrtc/#widl-RTCDataChannel-bufferedAmount
-  "getBufferedAmount": {type: "method",
-                        value: ["string"],
-                        ret: "number"},
+  'getBufferedAmount': {type: 'method',
+                        value: ['string'],
+                        ret: 'number'},
 
   // Returns local SDP headers from createOffer.
   'getInfo': {type: 'method', value: [], ret: 'string'},
 
   // Close the peer connection.
-  'close': {type: "method", value: []},
+  'close': {type: 'method', value: []},
   // The peer connection has been closed.
-  'onClose': {type: "event", value: {}}
+  'onClose': {type: 'event', value: {}}
 });
 
-fdom.apis.set("core.websocket", {
+fdom.apis.set('core.websocket', {
   // Constructs new websocket. Errors in construction are passed on
   // through the onError event.
   'constructor': {value:
     // URL to connect through
-    ["string",
+    ['string',
     // Protocols
-    ["array", "string"]]},
+    ['array', 'string']]},
   // Send the data to the other side of this connection. Only one of
   // the entries in the dictionary that is passed will be sent.
   'send': {
-    type: "method",
+    type: 'method',
     value: [{
-      "text": "string",
-      "binary": "blob",
-      "buffer": "buffer"
+      'text': 'string',
+      'binary': 'blob',
+      'buffer': 'buffer'
     }],
     err: {
-      "errcode": "string",
-      "message": "string"
+      'errcode': 'string',
+      'message': 'string'
     }
   },
   'getReadyState': {
-    type: "method",
+    type: 'method',
     value: [],
     // 0 -> CONNECTING
     // 1 -> OPEN
     // 2 -> CLOSING
     // 3 -> CLOSED
-    ret: "number"
+    ret: 'number'
   },
-  "getBufferedAmount": {type: "method",
-                        value: ["string"],
-                        ret: "number"},
+  'getBufferedAmount': {type: 'method',
+                        value: ['string'],
+                        ret: 'number'},
   'close': {
-    type: "method",
+    type: 'method',
     // The first argument is a status code from
     // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
-    value: ["number", "string"],
+    value: ['number', 'string'],
     err: {
-      "errcode": "string",
-      "message": "string"
+      'errcode': 'string',
+      'message': 'string'
     }
   },
   'onMessage': {
@@ -316,9 +316,9 @@ fdom.apis.set("core.websocket", {
     // The data will be stored in one of the keys,
     // corresponding with the type received
     value: {
-      "text": "string",
-      "binary": "blob",
-      "buffer": "buffer"
+      'text': 'string',
+      'binary': 'blob',
+      'buffer': 'buffer'
     }
   },
   'onOpen': {
@@ -328,8 +328,8 @@ fdom.apis.set("core.websocket", {
   'onError': {
     type: 'event',
     value: {
-      "errcode": "string",
-      "message": "string"
+      'errcode': 'string',
+      'message': 'string'
     }
   },
   'onClose': {
@@ -337,9 +337,9 @@ fdom.apis.set("core.websocket", {
     // Values given by WebSockets spec:
     // http://www.w3.org/TR/websockets/#closeevent
     value: {
-      "code": "number",
-      "reason": "string",
-      "wasClean": "boolean"
+      'code': 'number',
+      'reason': 'string',
+      'wasClean': 'boolean'
     }
   }
 });
