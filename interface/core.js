4fad85277f0b57139d65f092062054656e6fe847
Daniel Borkan <dborkan@google.com>
2014-10-03 13:13:35 -0400

    Update comment

--- interface/core.js
+++ interface/core.js
@@ -124,6 +124,9 @@ fdom.apis.set('core.tcpsocket', {
 
   // Prepares a socket for becoming secure after the next read event.
   // See details at https://github.com/uProxy/uproxy/issues/413
+  // This should be called one read prior to calling .secure, e.g. in XMPP
+  // this should be called before sending "starttls", then after a "proceed"
+  // message is read .secure should be called.
   'prepareSecure': {
     type: 'method',
     value: [],
