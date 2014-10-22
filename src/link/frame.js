2a8841f6b6d00b6fa84b637f08f4b228a7e2712d
Will Scott <willscott@gmail.com>
2014-10-21 21:51:14 -0400

    refactor frame to allow override of document.

--- src/link/frame.js
+++ src/link/frame.js
@@ -15,6 +15,17 @@ var Frame = function(id, resource) {
 };
 
 /**
+ * Get the document to use for the frame. This allows overrides in downstream
+ * links that want to essentially make an iFrame, but need to do it in another
+ * context.
+ * @method getDocument
+ * @protected
+ */
+Frame.prototype.getDocument = function () {
+  this.document = document;
+};
+
+/**
  * Start this port by listening or creating a frame.
  * @method start
  * @private
@@ -105,10 +116,10 @@ Frame.prototype.setupFrame = function() {
   var frame, onMsg;
   frame = this.makeFrame(this.config.source, this.config.inject);
   
-  if (!document.body) {
-    document.appendChild(document.createElement("body"));
+  if (!this.document.body) {
+    this.document.appendChild(this.document.createElement("body"));
   }
-  document.body.appendChild(frame);
+  this.document.body.appendChild(frame);
 
   onMsg = function(frame, msg) {
     if (!this.obj) {
@@ -128,7 +139,7 @@ Frame.prototype.setupFrame = function() {
     }
     this.revokeURL(frame.src);
     frame.src = "about:blank";
-    document.body.removeChild(frame);
+    this.document.body.removeChild(frame);
   };
 };
 
@@ -141,7 +152,7 @@ Frame.prototype.setupFrame = function() {
  */
 Frame.prototype.makeFrame = function(src, inject) {
   // TODO(willscott): add sandboxing protection.
-  var frame = document.createElement('iframe'),
+  var frame = this.document.createElement('iframe'),
       extra = '',
       loader,
       blob;
