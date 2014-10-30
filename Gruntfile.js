2b7a8afc867fc355a2c9a7825aee047262c5b611
Raymond Cheng <ryscheng@cs.washington.edu>
2014-10-30 08:11:56 -0700

    fixing grunt-bump config

--- Gruntfile.js
+++ Gruntfile.js
@@ -238,7 +238,7 @@ module.exports = function (grunt) {
         files: ['package.json', 'bower.json'],
         commit: true,
         commitMessage: 'Release v%VERSION%',
-        commitFiles: ['package.json'],
+        commitFiles: ['package.json', 'bower.json'],
         createTag: true,
         tagName: 'v%VERSION%',
         tagMessage: 'Version %VERSION%',
