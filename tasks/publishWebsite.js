60098a550e7936912f3b02b3638f77650991f393
Raymond Cheng <ryscheng@cs.washington.edu>
2014-12-21 16:38:00 -0500

    fixing bug in publishWebsite task

--- tasks/publishWebsite.js
+++ tasks/publishWebsite.js
@@ -6,14 +6,12 @@ module.exports = function (grunt) {
 
   grunt.registerTask('publishWebsite', 'Publishes to freedomjs.org.', function () {
     var done = this.async();
-    child_process.execFile('scripts/publishWebsite.sh', [], {
-      cwd: '../'
-    }, function(err, stdout, stderr) {
-      if (err) { throw err; }
+    child_process.execFile('scripts/publishWebsite.sh', [], function(err, stdout, stderr) {
+      if (err !== null) { grunt.log.error(err); }
       if (stdout) { grunt.log.writeln('out: ' + stdout.toString()); }
-      if (stderr) { grunt.log.writeln('err: ' + stderr.toString()); }
+      if (stderr) { grunt.log.error('err: ' + stderr.toString()); }
       grunt.log.writeln('Done publishing to freedomjs.org');
       done();
-    });
+    }.bind(this));
   });
 };
