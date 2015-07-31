0bf96ea19eb677171fabbf78f38b85d9780c5b34
dborkan <dborkan@gmail.com>
2015-07-31 15:36:56 -0400

    Abort oauth flow after 5 seconds if interactive==false and no redirect

--- providers/oauth/oauth.localpageauth.js
+++ providers/oauth/oauth.localpageauth.js
@@ -65,7 +65,7 @@ LocalPageAuth.prototype.initiateOAuth = function(redirectURIs, continuation) {
  * @method launchAuthFlow
  * @param {String} authUrl - The URL that initiates the auth flow.
  * @param {Object.<string, string>} stateObj - The return value from initiateOAuth
- * @param {Boolean} interactive - Whether to launch an interactive flow (ignored)
+ * @param {Boolean} interactive - Whether to launch an interactive flow
  * @param {Function} continuation - Function to call when complete
  *    Expected to see a String value that is the response Url containing the access token
  */
@@ -76,6 +76,18 @@ LocalPageAuth.prototype.launchAuthFlow = function(authUrl, stateObj, interactive
   window.addEventListener("storage", listener, false);
   // Start 'er up
   window.open(authUrl);
+
+  if (interactive === false) {
+    setTimeout(function() {
+      if (this.listeners[stateObj.state]) {
+        // Listener has not been deleted, indicating oauth has completed.
+        window.removeEventListener(
+            "storage", this.listeners[stateObj.state], false);
+        delete this.listeners[stateObj.state];
+        continuation(undefined, 'Error launching auth flow');
+      }
+    }.bind(this), 5000);
+  }
 };
 
 /**
@@ -84,6 +96,7 @@ LocalPageAuth.prototype.launchAuthFlow = function(authUrl, stateObj, interactive
  * http://tutorials.jenkov.com/html5/local-storage.html#storage-events
  * @param {Function} continuation function to call with result
  * @param {Object.<string, string>} stateObj the return value from initiateOAuth
+
  * @param {Object} msg storage event
  */
 LocalPageAuth.prototype.storageListener = function(continuation, stateObj, msg) {
